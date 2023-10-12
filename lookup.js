e1b77febcad33f9577021b3225b3daf73701a24a
Krystian Czajkowski <36819859+krystianczajkowski@users.noreply.github.com>
2023-10-12 07:31:29 +0000

    lookup is returning valid urls

--- lookup.js
+++ lookup.js
@@ -1,10 +1,12 @@
 
 
 function lookup(symbol) {
-  ticker = symbol.toUpperCase();
-  timeNow = Date.now();
-  timeThen = new Date (timeNow - (60 * 60 * 1000));
-  return [new Date(timeNow), timeThen];
+  let ticker = symbol.toUpperCase();
+  let timeNow = parseInt(Date.now()/1000);
+  let timeThen = parseInt(timeNow - (60 * 60 * 1000));
+  let url = `https://query1.finance.yahoo.com/v7/finance/download/${symbol}?period1=${timeThen}&period2=${timeNow}&interval=1d&events=history&includeAdjustedClose=true`;
+  
+  return [timeNow, timeThen, url];
 }
 
 console.log(lookup('nflx'));
