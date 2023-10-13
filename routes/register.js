1de945fb7fad4344a8b4e1d027c11cf6a87b4b3c
Krystian Czajkowski <36819859+krystianczajkowski@users.noreply.github.com>
2023-10-13 19:36:28 +0000

    feat: password hashing and checking hashes

--- routes/register.js
+++ routes/register.js
@@ -1,21 +1,50 @@
+var sqlite3 = require('sqlite3').verbose();
 var express = require('express');
+const session = require('express-session');
 var router = express.Router();
+var crypto = require('crypto');
 
+function checkPasswordHash(hash, salt, pass) {
+  return hash === crypto.pbkdf2Sync(pass, salt, 1000, 64, `sha512`).toString(`hex`);
+}
 
+const db = new sqlite3.Database('database.db');
+
+let fetchUsernames = `SELECT username, hash, salt FROM users WHERE username = ?`;
+let insertUser = `INSERT INTO users(username, hash) VALUES(?, ?)`;
 
 /* GET home page. */
-router.post('/register', async function(req, res, next) {
-  let formAnswer = req.body.userAnswer;
+router.post('/', async function(req, res, next) {
+  // sessionStorage.clear();
+  let username = req.body.username;
+  let password = req.body.password;
+  let confirmation = req.body.confirmation;
+  if (password != confirmation) {
+    res.render('register.njk', message = 'Passwords must match!')
+  }
+  let salt = crypto.randomBytes(16).toString('hex');
+  let hash = crypto.pbkdf2Sync(password, salt, 
+    1000, 64, `sha512`).toString(`hex`);
+  let checkedHash = checkPasswordHash(hash, salt, password);
+  db.each(fetchUsernames, [username], (err, row) => {
+    if (err) {
+      throw err;
+    }
+    if (!row | checkPasswordHash())
+    console.log(`Username: ${row.username}`);
+  });
+
   let  data = {
-    message: formAnswer,
+    message: checkedHash,
     title: 'register',
   };
-
-  res.render('login.njk', data);
+  
+  res.render('register.njk', data);
 });
 
 router.get('/', function(req, res) {
-  res.render('index.njk', {title: 'REGISTER', message: 'hello stranger'});
+  res.render('register.njk', {title: 'REGISTER', message: 'hello stranger'});
 });
 
+db.close();
 module.exports = router;
