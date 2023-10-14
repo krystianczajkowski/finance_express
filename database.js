7ec216ffdae4e204e15e5ab123ce5350c484ce35
Krystian Czajkowski <36819859+krystianczajkowski@users.noreply.github.com>
2023-10-14 13:28:46 +0000

    feat: database connection for the app

---
+++ database.js
@@ -0,0 +1,8 @@
+var sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('database.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return console.error(err.message);
+    console.log('connection succesfull');
+});
+
+module.exports = db;
