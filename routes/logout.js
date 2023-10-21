0ef85d47478aacf1255d72539b5f9bd1ddb80bd6
Krystian Czajkowski <36819859+krystianczajkowski@users.noreply.github.com>
2023-10-21 19:51:39 +0000

    delete blank line

--- routes/logout.js
+++ routes/logout.js
@@ -11,7 +11,6 @@ router.get('/', function (req, res) {
   req.session.user = null;
   req.session.save(function (err) {
     if (err) next(err);
-    
     // regenerate the session, which is good practice to help
     // guard against forms of session fixation
     req.session.regenerate(function (err) {
