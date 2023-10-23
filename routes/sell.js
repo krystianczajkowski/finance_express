1dec8fe9c40e1bcb2bf168fb69134522e6474ef1
Krystian Czajkowski <36819859+krystianczajkowski@users.noreply.github.com>
2023-10-23 04:51:14 +0000

    database and session stuff

--- routes/sell.js
+++ routes/sell.js
@@ -1,18 +1,26 @@
 var express = require("express");
 var router = express.Router();
 var auth = require("../auth");
+var db = require("../database");
 
-/* GET home page. */
-router.post("/sell", auth, async function (req, res, next) {
-    let data = {
-        message: "Sold ",
-        title: "SELL",
-    };
+var fetchUserData = `SELECT * FROM users WHERE username=?`;
+var setCash = `UPDATE users SET cash=? WHERE username=?`;
+var logTransaction = `INSERT INTO transactions(user_id, stock_value, stock, quantity, transaction_type) VALUES(?, ?, ?, ?, ?)`;
+var getUserStock = `SELECT `
 
-    res.render("index.njk", data);
+router.post("/sell", auth, function (req, res, next) {
+    db.get(fetchUserData, [req.session.user], function(err, row) {
+        if (err) console.error(err.message);
+    });
+    res.render("sell.njk", data);
 });
 
 router.get("/", auth, function (req, res) {
+    // count how many stocks a user has
+    db.get(fetchUserData, [req.session.user], function(err, row) {
+        if (err) console.error(err.message);
+
+    });
     res.render("sell.njk", { title: "SELL", message: "Sell stock" });
 });
 
