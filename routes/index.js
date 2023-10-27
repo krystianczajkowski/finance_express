b26a21a0705de36bca243fc337218554951cde03
Krystian Czajkowski <KrystianCzajkowski01@gmail.com>
2023-10-27 16:16:00 +0200

    add auth to index page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
+const auth = require('../auth');
+const db = require('../database');
 var router = express.Router();
 
 
 
 /* GET home page. */
-router.post('/',  function(req, res, next) {
+router.post('/', auth, function(req, res, next) {
   let formAnswer = req.body.userAnswer;
   let  data = {
     message: `Hello ${req.session.user}`,
@@ -14,8 +16,23 @@ router.post('/',  function(req, res, next) {
   res.render('index.njk', data);
 });
 
-router.get('/', function(req, res) {
-  res.render('index.njk', {title: 'INDEX', message: `Hello ${req.session.user}`});
+router.get('/', auth, function(req, res) {
+  if (req.session.user) {
+    db.all("SELECT username FROM users", function(err, rows) {
+      if (err) console.error(err.message);
+      let data = {
+        message: `Hello ${req.session.user}`,
+        title: 'Index',
+      };
+      // for (let i = 0; i < rows.length; i++) {
+      //   data['users'][i] = rows[i]['username'];
+      //   console.log(data['users']);
+      // }
+      res.render('index.njk', data);
+    });
+    return 0;
+  }
+  return res.render('index.njk', {message: 'Hello stranger'})
 });
 
 module.exports = router;
