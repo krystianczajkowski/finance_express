08265226f670a0ec135b34534bdce3149ac6c377
Krystian Czajkowski <36819859+krystianczajkowski@users.noreply.github.com>
2023-10-31 20:14:53 +0100

    add users route functionality (list all users)

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var auth = require('../auth')
+var auth = require('../auth');
+var db = require("../database");
 
-/* GET users listing. */
-router.get('/', auth, function(req, res, next) {
-  res.render('users.njk', {title: 'Users', message: 'No users present!', session: true});
+const fetchUsernames = `SELECT username FROM users`;
+
+/* GET home page. */
+router.get('/', auth, function(req, res) {
+  db.all(fetchUsernames, (err, rows) => {
+    if (err) console.error(err.message);
+    let allUsers = {};
+    for (let i = 0; i < rows.length; i++) {
+      const element = rows[i];
+      allUsers[i] = element['username'];
+      console.log(allUsers[i]);
+    }
+    res.render('users.njk', {title: 'Users', message: 'All users by account age:', users: allUsers});
+  })
 });
 
 module.exports = router;
