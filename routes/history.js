8eb702a4478927196c4aca0f9abf73853571d743
Krystian Czajkowski <36819859+krystianczajkowski@users.noreply.github.com>
2023-10-31 21:08:12 +0100

    implement user transaction history

--- routes/history.js
+++ routes/history.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var auth = require('../auth');
+var db = require('../database');
 
+const getHistory = `SELECT up.user_id, up.stock, up.stock_value, up.quantity, up.transaction_date, up.transaction_type FROM user_portfolio up JOIN users u ON up.user_id = u.id WHERE u.username = ?`;
 
 /* GET home page. */
 router.get('/', auth, function(req, res) {
-  res.render('history.njk', {title: 'HISTORY', message: 'your history', session: true});
+  db.all(getHistory, [req.session.user], function(err, rows) {
+    if (err) console.error(err.message);
+    console.log(rows);
+    res.render('history.njk', { title: 'History', message: 'Your history', session: true, transactions: rows.reverse() });
+  });
 });
 
 module.exports = router;
