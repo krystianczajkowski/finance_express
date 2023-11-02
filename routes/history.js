8cf8d59498245cefba3bf16b57a0ba14d6fad92d
Krystian Czajkowski <36819859+krystianczajkowski@users.noreply.github.com>
2023-11-02 15:20:49 +0100

    add check if user has history

--- routes/history.js
+++ routes/history.js
@@ -9,7 +9,15 @@ const getHistory = `SELECT up.user_id, up.stock, up.stock_value, up.quantity, up
 router.get('/', auth, function(req, res) {
   db.all(getHistory, [req.session.user], function(err, rows) {
     if (err) console.error(err.message);
-    console.log(rows);
+    if (rows.length == 0) {
+      let data = {
+        title: 'History',
+        message: 'You have no history',
+        session: 'true',
+        transactions: null
+      };
+      return res.render('history.njk', data);
+    }
     res.render('history.njk', { title: 'History', message: 'Your history', session: true, transactions: rows.reverse() });
   });
 });
